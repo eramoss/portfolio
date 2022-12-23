8a361e8e1b47bbc824b393fbcf2e153d7f94c9ef
eramoss <edulramos@outlook.com>
2022-12-23 19:59:25 -0300

    change CV

--- components/headerComponent/Menu.tsx
+++ components/headerComponent/Menu.tsx
@@ -1,43 +1,33 @@
-import React, { useContext } from 'react';
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
-import {
-  HiMenuAlt3,
-
-} from 'react-icons/hi';
-
-
-
-
-
-
+import React, { useContext } from "react";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 const Menu = () => {
-
-
-
-  
-    
-
-
-
   return (
     <DropdownMenu.Root>
-      
-      <DropdownMenu.Trigger className='flex items-center gap-1 ' asChild>
-        
-        <button id='btn-menu' className="IconButton" aria-label="Customise options">
-          <label className='' htmlFor="btn-menu">Menu</label>
-          <HiMenuAlt3/>
+      <DropdownMenu.Trigger className="flex items-center gap-1 " asChild>
+        <button
+          id="btn-menu"
+          className="IconButton"
+          aria-label="Customise options"
+        >
+          <label className="" htmlFor="btn-menu">
+            Menu
+          </label>
+          <HiMenuAlt3 />
         </button>
       </DropdownMenu.Trigger>
-      
 
       <DropdownMenu.Portal>
-        <DropdownMenu.Content className="w-56 absolute md:top-0  -right-12 md:-right-8 rounded-md bg-gray-100 p-2 shadow-md duration-500 transition transform flex flex-col items-center gap-3" sideOffset={5}>
-            <a href='/curriculo.pdf' download >Baixar CV</a>
-            <a href="https://www.linkedin.com/in/eramoss/">LinkedIn</a>
-            <a href="https://github.com/eramoss">Repositórios</a>
-          
+        <DropdownMenu.Content
+          className="w-56 absolute md:top-0  -right-12 md:-right-8 rounded-md bg-gray-100 p-2 shadow-md duration-500 transition transform flex flex-col items-center gap-3"
+          sideOffset={5}
+        >
+          <a href="/Curriculo-eduardo-ramos.pdf" download>
+            Baixar CV
+          </a>
+          <a href="https://www.linkedin.com/in/eramoss/">LinkedIn</a>
+          <a href="https://github.com/eramoss">Repositórios</a>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
