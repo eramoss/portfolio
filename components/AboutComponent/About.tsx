d194b188d92dfa7544bf37c9e187e3187a86235a
eramoss <edulramos@outlook.com>
2023-10-19 12:28:48 -0300

    remove error "A little more about 'mes'"

--- components/AboutComponent/About.tsx
+++ components/AboutComponent/About.tsx
@@ -3,7 +3,7 @@ import Text from "./Text";
 export default function About() {
   return (
     <div className='flex flex-col items-center my-36 w-full'>
-        <h1 className="text-2xl font-medium" >A little more about mes</h1>
+        <h1 className="text-2xl font-medium" >A little more about me</h1>
         <Text/>
     </div>
   )
